5eeeae66be7518a118500f424c8e6dbd7cf6967d
Glitch (remarkable-sailboat) <none>
2018-12-21 18:25:29 +0000

    💵⚽️ Checkpoint ./routes/api.js:2265073/31 ./server.js:2265073/342 ./package.json:2265073/28 ./public/forms.js:2265073/6 ./views/index.html:2265073/9

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const cors        = require('cors');
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
+const mongoose = require('mongoose');
 
 const helmet = require('helmet');
 const app = express();
@@ -51,34 +52,38 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);
+mongoose.connect(process.env.DB, {useNewUrlParser: true}, (err, db) => {
+  
+  //Routing for API 
+  apiRoutes(app);
 
-//Sample Front-end
+  //Sample Front-end
 
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+
+  //404 Not Found Middleware
+  app.use(function(req, res, next) {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+
+  //Start our server and tests!
+  app.listen(process.env.PORT || 3000, function () {
+    console.log("Listening on port " + process.env.PORT);
+    if(process.env.NODE_ENV==='test') {
+      console.log('Running Tests...');
+      setTimeout(function () {
+        try {
+          runner.run();
+        } catch(e) {
+          var error = e;
+            console.log('Tests are not valid:');
+            console.log(error);
+        }
+      }, 1500);
+    }
+  });
+
+  module.exports = app; //for testing
+
 });
-
-//Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 1500);
-  }
-});
-
-module.exports = app; //for testing
