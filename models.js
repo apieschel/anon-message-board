0c86b85da142c371fdca99ae5b659697ef4bce69
Glitch (remarkable-sailboat) <none>
2018-12-22 19:59:22 +0000

    🚉🐺 Checkpoint ./models.js:2265073/540 ./public/thread.js:2265073/25 ./public/board.js:2265073/25

--- models.js
+++ models.js
@@ -1,17 +1,19 @@
 let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
+let childSchema = new Schema({
+    text: String, 
+    delete_password: String, 
+    thread_id: String
+}, {timestamps: true});
+                        
 let threadSchema = new Schema({
 	title: String,
 	text: String,
   password: String,
   board: String,
   reported: Boolean,
-  replies: [{
-    text: String, 
-    delete_password: String, 
-    thread_id: String
-  }, {timestamps: true}],
+  replies: [childSchema],
   replycount: Number
 }, {timestamps: true});
 
