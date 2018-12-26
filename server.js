f73c0f45a3d992a6f16bdf98e4c7b52c144870d3
Glitch (remarkable-sailboat) <none>
2018-12-26 23:19:08 +0000

    🎊⚽️ Checkpoint ./server.js:2265073/1391

--- server.js
+++ server.js
@@ -58,36 +58,39 @@ fccTestingRoutes(app);
 
 mongoose.connect(process.env.DB, {useNewUrlParser: true}, (err, db) => {
   
-  //Routing for API 
-  apiRoutes(app);
+  if(err) {
+    console.log('Database error: ' + err);
+  } else {
+    
+    console.log('Successful database connection');
+      
+    //Routing for API 
+    apiRoutes(app);
 
-  //Sample Front-end
+    //404 Not Found Middleware
+    app.use(function(req, res, next) {
+      res.status(404)
+        .type('text')
+        .send('Not Found');
+    });
 
+    //Start our server and tests!
+    app.listen(process.env.PORT || 3000, function () {
+      console.log("Listening on port " + process.env.PORT);
+      if(process.env.NODE_ENV==='test') {
+        console.log('Running Tests...');
+        setTimeout(function () {
+          try {
+            runner.run();
+          } catch(e) {
+            var error = e;
+              console.log('Tests are not valid:');
+              console.log(error);
+          }
+        }, 1500);
+      }
+    });
 
-  //404 Not Found Middleware
-  app.use(function(req, res, next) {
-    res.status(404)
-      .type('text')
-      .send('Not Found');
-  });
-
-  //Start our server and tests!
-  app.listen(process.env.PORT || 3000, function () {
-    console.log("Listening on port " + process.env.PORT);
-    if(process.env.NODE_ENV==='test') {
-      console.log('Running Tests...');
-      setTimeout(function () {
-        try {
-          runner.run();
-        } catch(e) {
-          var error = e;
-            console.log('Tests are not valid:');
-            console.log(error);
-        }
-      }, 1500);
-    }
-  });
-
-  module.exports = app; //for testing
-
+    module.exports = app; //for testing
+  }
 });
